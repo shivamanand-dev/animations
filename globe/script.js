0ac223c0622b645029b2dae46d8c307e0044efd6
shivam0anand <shivam_anand@outlook.com>
2020-08-13 20:00:36 +0530

    modified

--- globe/script.js
+++ globe/script.js
@@ -1,1 +1,11 @@
-alert("lets go");
+const wrap = document.getElementById("wrap");
+
+const particles = () => {
+  for (i = 0; i < 300; i++) {
+    let createDiv = document.createElement("div");
+    wrap.appendChild(createDiv);
+    createDiv.classList.add("c");
+  }
+};
+
+particles();
